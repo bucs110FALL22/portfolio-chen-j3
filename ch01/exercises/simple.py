7ce062151083b4118229b0af25f3e8f884dcf1df
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-09-06 21:56:54 +0000

    working with data exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,19 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%10)
+print(0%10)
+print(10/15)
+#What is wrong with the last answer?
+#The last answer is 0.666666. The 6 is repeating.
+
+#part 2
+
+rate = int(input("What is the exchange rate for the Euro to Dollar?\n"))
+amount = int(input("What is the amount of currency you need to exchange?\n"))
+total = rate*amount
+result = total-3
+print("Your result is $",result)
--- main.py
+++
@@ -1,3 +0,0 @@
-print("Hello world")
-#comment
-# highlight and ctrl / to comment multiple lines
