2be381466d4fa6fa7e66410be3fab1e48e184fcb
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-09-09 19:29:47 +0000

    chapter 2 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -6,5 +6,15 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
+cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class))
+print("Your cost per class is $", cost_per_class)
 
 #Part B
+import random
+#random.randrange(1,10)
+dataObjects = [1, 1/5, "stem", 888, 23/2]
+randomObj = random.choice(dataObjects)
+print("Your random object is", randomObj)
