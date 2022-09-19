40dceac9fef28ef61e6a88e580d180acfd73642b
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-09-19 22:25:50 +0000

    new repl join link

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,12 +1,42 @@
 import random
 
-answer = random.randrange(1,11)
+num = random.randrange(1,11)
+num_guesses = 1
 
 for i in range(3):
-  guess = int(input("Guess the number from 1-10."))
-  if guess == answer:
+  guess_num = int(input("Please enter a guess:"))
+  #num_guesses = num_guesses + 1
+  num_guesses +=1
+  if guess_num > num:
+      print("your guess is too high")
+  elif guess_num < num:
+      print("your guess is too low")
+  else:
     print("Correct!")
-    elif guess > answer:
-      print("Too high.")
-    elif guess < answer:
-      print("Too low.")
+  break
+  
+print("It took you", num_guesses, "to get it right.")
+
+# import random
+
+# answer = random.randrange(1,11) #gives one number between the range
+# num_guesses = 0 #to keep track of number of guesses
+
+# for i in range(3): #use range to create list . list of 3 things - loop 3 times
+#   #i is a variable name 
+#   guess = int(input("Guess the number from 1-10."))
+#   if guess == answer:
+#     print("Correct!")
+#     elif guess > answer:
+#       print("Too high.")
+#     else:
+#       print("Too low.")
+
+#       break
+# print ("It took you" num_guesses, "to guess the correct number.")
+
+
+# #to end loop - flag variable approach
+# # correct_guess = False (before i in range line)
+
+# #other approach - break
