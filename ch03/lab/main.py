62ad6870b80bdb68ab0e6d728866055dbd8c9615
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-09-23 17:35:53 +0000

    ch03 lab part a uncommented

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -4,39 +4,39 @@ import pygame
 import math
 
 # #Part A
-# window = turtle.Screen() # 2.  Create a screen
-# window.bgcolor('lightblue')
+window = turtle.Screen() # 2.  Create a screen
+window.bgcolor('lightblue')
 
-# michelangelo = turtle.Turtle() # 3.  Create two turtles
-# leonardo = turtle.Turtle()
-# michelangelo.color('orange')
-# leonardo.color('blue')
-# michelangelo.shape('turtle')
-# leonardo.shape('turtle')
-# michelangelo.up() # 4. Pick up the pen so we don’t get lines
-# leonardo.up()
-# michelangelo.goto(-100,20)
-# leonardo.goto(-100,-20)
-# #own code to change the speed of turtles
-# leonardo.speed(1)
-# michelangelo.speed(1)
+michelangelo = turtle.Turtle() # 3.  Create two turtles
+leonardo = turtle.Turtle()
+michelangelo.color('orange')
+leonardo.color('blue')
+michelangelo.shape('turtle')
+leonardo.shape('turtle')
+michelangelo.up() # 4. Pick up the pen so we don’t get lines
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+#own code to change the speed of turtles
+leonardo.speed(1)
+michelangelo.speed(1)
 
-# ## 5. Your PART A code goes here
-# # part A pt 1
-# distance = random.randrange(1,101)
-# leonardo.forward(distance)
-# michelangelo.forward(distance)
-# leonardo.goto(-100,20)
-# michelangelo.goto(-100,-20)
+## 5. Your PART A code goes here
+# part A pt 1
+distance = random.randrange(1,101)
+leonardo.forward(distance)
+michelangelo.forward(distance)
+leonardo.goto(-100,20)
+michelangelo.goto(-100,-20)
 
-# # part A pt 2
-# for i in range(10):
-#   distanceTwo = random.randrange(0,11)
-#   leonardo.forward(distanceTwo)
-#   michelangelo.forward(distanceTwo)
+# part A pt 2
+for i in range(10):
+  distanceTwo = random.randrange(0,11)
+  leonardo.forward(distanceTwo)
+  michelangelo.forward(distanceTwo)
   
-# leonardo.goto(-100,20)
-# michelangelo.goto(-100,-20)
+leonardo.goto(-100,20)
+michelangelo.goto(-100,-20)
 
 
 # PART B - complete part B here
