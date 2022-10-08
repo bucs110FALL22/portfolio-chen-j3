849fd8eabd52b2ed6b94616f38705ed1f4d6f622
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-10-08 03:44:50 +0000

    chapter 4 lab (unfinished version)

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -8,9 +8,11 @@ screen = pygame.display.set_mode()
 pygame.FULLSCREEN
 size = pygame.display.get_window_size()
 
+#draw circle and lines
 screen.fill("cornflowerblue")
+pygame.draw.circle()
 
 #part b (throwing darts)
-
+distance_from_center = math.hypot()
 
 #part c (game of darts)
