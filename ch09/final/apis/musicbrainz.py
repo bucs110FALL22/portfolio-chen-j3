4811cd44559a7835cfddfb1e3903dd5dde72e35e
jchen753 <109445832+chen-j3@users.noreply.github.com>
2022-12-13 01:29:11 +0000

    api final jc cs110

--- ch09/final/apis/musicbrainz.py
+++ ch09/final/apis/musicbrainz.py
@@ -2,16 +2,16 @@ import requests as R
 
 class MusicbrainzAPI:
     def __init__(self, artistName):
-    """
-    transfers randomized artist name (string) to musicbrainz api
-    """
+        """
+      transfers randomized artist name (string) to musicbrainz api
+      """
         self.url = f"https://musicbrainz.org/ws/2/artist?query={artistName}&fmt=json"
         self.headers = {
             "Content-Type": "application/json"
         }
 
     def get(self):
-      """
+        """
       returns response from musicbrainz api (information on randomized artist)
       """
         response = R.get(
--- ch09/final/apis/spotify.py
+++ ch09/final/apis/spotify.py
@@ -3,13 +3,17 @@ import requests as R
 class SpotifyAPI:
     def __init__(self, access_token):
         """
-      This endpoint logs out the current user by emptying the token
+      takes access_token (string) to initialize spotify api
       """
         self.headers = {
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json"
         }
+      
     def get_artist(self, id):
+        """
+        uses id (integer) to return the artist from spotify api
+        """
         response = R.get(
             f"https://api.spotify.com/v1/artists/{id}", 
             headers=self.headers
@@ -17,6 +21,9 @@ class SpotifyAPI:
         return response.json()
       
     def get_profile(self):
+        """
+        returns the spotify user profile
+        """
         response = R.get(
             "https://api.spotify.com/v1/me", 
             headers=self.headers
@@ -24,6 +31,9 @@ class SpotifyAPI:
         return response.json()
 
     def get_top_artists(self):
+        """
+        returns the information of the user's top spotify artists
+        """
         response = R.get(
             "https://api.spotify.com/v1/me/top/artists",
             headers=self.headers
